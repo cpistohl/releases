70030ceb851b31f211647c7336f28a11ab07ea43
Connor Pistohl <cpistohl@icloud.com>
2026-02-28 13:10:52 -0600

    refactor: update Azure Key Vault name and modify deployment credentials in workflow

--- .config/main.tf
+++ .config/main.tf
@@ -59,7 +59,7 @@ data "azurerm_container_app_environment" "main" {
 # --- App-specific resources ---
 
 resource "azurerm_key_vault" "main" {
-  name                = "kv-releases-cpistohl"
+  name                = "kv-apps-cpistohl"
   location            = data.azurerm_resource_group.main.location
   resource_group_name = data.azurerm_resource_group.main.name
   tenant_id           = data.azurerm_client_config.current.tenant_id
